3dfe7349f309814a2e69118332e59afbdb3d6e80
JavaScript <243579780@qq.com>
2020-11-29 09:32:00 +0800

    Add files via upload

---
+++ wxUI/pages/flex/flex.js
@@ -0,0 +1,17 @@
+// pages/button/button.js
+Page({
+    data: {},
+    showActionSheet:function(){
+        wx.showActionSheet({
+            itemList: ['1','2'],
+        })
+    },
+    onLoad: function (options) { },
+    onReady: function () { },
+    onShow: function () { },
+    onHide: function () { },
+    onUnload: function () { },
+    onPullDownRefresh: function () { },
+    onReachBottom: function () { },
+    onShareAppMessage: function () { }
+})
